173a6e541e6fafed70620a0339ff2f097a00413a
D3stros <patrick.fallmann@hotmail.com>
2020-05-20 17:02:44 +0200

    Created spring boot file structure and dependencies via Spring Initializr

---
+++ src/main/java/com/Gamestalk/springnggamestalk/SpringNgGamestalkApplication.java
@@ -0,0 +1,13 @@
+package com.Gamestalk.springnggamestalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNgGamestalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNgGamestalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gamestalk/springnggamestalk/SpringNgGamestalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gamestalk.springnggamestalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgGamestalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
