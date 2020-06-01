4b412b61e22b5833eab5cb779374ab57c5a51f23
D3stros <patrick.fallmann@hotmail.com>
2020-06-01 17:18:56 +0200

    Creating GET endpoint to verify users in the AuthController

--- src/main/java/com/Gamestalk/springnggamestalk/controller/AuthController.java
+++ src/main/java/com/Gamestalk/springnggamestalk/controller/AuthController.java
@@ -4,10 +4,7 @@ import com.Gamestalk.springnggamestalk.dto.RegisterRequest;
 import com.Gamestalk.springnggamestalk.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -22,5 +19,11 @@ public class AuthController {
     public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
         return new ResponseEntity(OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successully", OK);
     }
 }
