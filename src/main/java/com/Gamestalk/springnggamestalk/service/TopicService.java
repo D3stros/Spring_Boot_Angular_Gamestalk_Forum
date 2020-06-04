c037684659621f8dd29ae96d40346b6b60369e96
D3stros <patrick.fallmann@hotmail.com>
2020-06-04 16:48:19 +0200

    Implemented mapper inside TopicService

--- src/main/java/com/Gamestalk/springnggamestalk/service/TopicService.java
+++ src/main/java/com/Gamestalk/springnggamestalk/service/TopicService.java
@@ -1,58 +1,48 @@
 package com.Gamestalk.springnggamestalk.service;
 
 import com.Gamestalk.springnggamestalk.dto.TopicDto;
+import com.Gamestalk.springnggamestalk.exception.SpringGamestalkException;
 import com.Gamestalk.springnggamestalk.exception.TopicNotFoundException;
+import com.Gamestalk.springnggamestalk.mapper.TopicMapper;
 import com.Gamestalk.springnggamestalk.model.Topic;
 import com.Gamestalk.springnggamestalk.repository.TopicRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static java.time.Instant.now;
 import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class TopicService {
 
     private final TopicRepository topicRepository;
-    private final AuthService authService;
+    private final TopicMapper topicMapper;
+
+    @Transactional
+    public TopicDto save(TopicDto topicDto) {
+        Topic topic = topicRepository.save(topicMapper.mapDtoToTopic(topicDto));
+        topicDto.setId(topic.getId());
+        return topicDto;
+    }
+
 
     @Transactional(readOnly = true)
     public List<TopicDto> getAll() {
         return topicRepository.findAll()
                 .stream()
-                .map(this::mapToDto)
+                .map(topicMapper::mapTopicToDto)
                 .collect(toList());
     }
 
-    @Transactional
-    public TopicDto save(TopicDto topicDto) {
-        Topic topic = topicRepository.save(mapToTopic(topicDto));
-        topicDto.setId(topic.getId());
-        return topicDto;
-    }
-
-    @Transactional(readOnly = true)
     public TopicDto getTopic(Long id) {
         Topic topic = topicRepository.findById(id)
-                .orElseThrow(() -> new TopicNotFoundException("Topic not found with id -" + id));
-        return mapToDto(topic);
-    }
-
-    private TopicDto mapToDto(Topic topic) {
-        return TopicDto.builder().name(topic.getName())
-                .id(topic.getId())
-                .numberOfPosts(topic.getPosts().size())
-                .build();
-    }
-
-    private Topic mapToTopic(TopicDto topicDto) {
-        return Topic.builder().name("/r/" + topicDto.getName())
-                .description(topicDto.getDescription())
-                .user(authService.getCurrentUser())
-                .createdDate(now()).build();
+                .orElseThrow(() -> new SpringGamestalkException("No topic found with ID - " + id));
+        return topicMapper.mapTopicToDto(topic);
     }
 }
+
