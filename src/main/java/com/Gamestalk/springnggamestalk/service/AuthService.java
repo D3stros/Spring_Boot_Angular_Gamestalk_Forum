051aa45d4e4202ecde62a2ac6793631ee70c0100
D3stros <patrick.fallmann@hotmail.com>
2020-06-06 16:42:46 +0200

    Implemented logout via refresh tokens

--- src/main/java/com/Gamestalk/springnggamestalk/service/AuthService.java
+++ src/main/java/com/Gamestalk/springnggamestalk/service/AuthService.java
@@ -2,6 +2,7 @@ package com.Gamestalk.springnggamestalk.service;
 
 import com.Gamestalk.springnggamestalk.dto.AuthenticationResponse;
 import com.Gamestalk.springnggamestalk.dto.LoginRequest;
+import com.Gamestalk.springnggamestalk.dto.RefreshTokenRequest;
 import com.Gamestalk.springnggamestalk.dto.RegisterRequest;
 import com.Gamestalk.springnggamestalk.exception.SpringGamestalkException;
 import com.Gamestalk.springnggamestalk.model.NotificationEmail;
@@ -11,7 +12,6 @@ import com.Gamestalk.springnggamestalk.repository.UserRepository;
 import com.Gamestalk.springnggamestalk.repository.VerificationTokenRepository;
 import com.Gamestalk.springnggamestalk.security.JwtProvider;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,56 +20,47 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
-
-import static com.Gamestalk.springnggamestalk.util.Constants.ACTIVATION_EMAIL;
-import static java.time.Instant.now;
-
 @Service
 @AllArgsConstructor
-@Slf4j
+@Transactional
 public class AuthService {
-    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
-    private final MailContentBuilder mailContentBuilder;
     private final MailService mailService;
-    private AuthenticationManager authenticationManager;
-    private JwtProvider jwtProvider;
-
-    @Transactional
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(encodePassword(registerRequest.getPassword()));
-        user.setCreated(now());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
         user.setEnabled(false);
         userRepository.save(user);
         String token = generateVerificationToken(user);
-        String message = mailContentBuilder.build("Thank you for signing up to Gamestalk, please click on the below url to activate your account : "
-                + ACTIVATION_EMAIL + "/" + token);
-        mailService.sendMail(new NotificationEmail("Please Activate your account", user.getEmail(), message));
+        mailService.sendMail(new NotificationEmail("Please Activate your Account",
+                user.getEmail(), "Thank you for signing up to Gamestalk, " +
+                "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
     }
-
     @Transactional(readOnly = true)
-    User getCurrentUser() {
+    public User getCurrentUser() {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.
                 getContext().getAuthentication().getPrincipal();
         return userRepository.findByUsername(principal.getUsername())
                 .orElseThrow(() -> new UsernameNotFoundException("User name not found - " + principal.getUsername()));
     }
-
-    @Transactional
     private void fetchUserAndEnable(VerificationToken verificationToken) {
         String username = verificationToken.getUser().getUsername();
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringGamestalkException("User Not Found with id - " + username));
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringGamestalkException("User not found with name - " + username));
         user.setEnabled(true);
         userRepository.save(user);
     }
-
     private String generateVerificationToken(User user) {
         String token = UUID.randomUUID().toString();
         VerificationToken verificationToken = new VerificationToken();
@@ -78,24 +69,30 @@ public class AuthService {
         verificationTokenRepository.save(verificationToken);
         return token;
     }
-
     public void verifyAccount(String token) {
-        Optional<VerificationToken> verificationTokenOptional = verificationTokenRepository.findByToken(token);
-        verificationTokenOptional.orElseThrow(() -> new SpringGamestalkException("Invalid Token"));
-        fetchUserAndEnable(verificationTokenOptional.get());
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        fetchUserAndEnable(verificationToken.orElseThrow(() -> new SpringGamestalkException("Invalid Token")));
     }
-
-
     public AuthenticationResponse login(LoginRequest loginRequest) {
         Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
                 loginRequest.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authenticate);
-        String authenticationToken = jwtProvider.generateToken(authenticate);
-        return new AuthenticationResponse(authenticationToken, loginRequest.getUsername());
+        String token = jwtProvider.generateToken(authenticate);
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .refreshToken(refreshTokenService.generateRefreshToken().getToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(loginRequest.getUsername())
+                .build();
     }
-
-    private String encodePassword(String password) {
-        return passwordEncoder.encode(password);
+    public AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.validateRefreshToken(refreshTokenRequest.getRefreshToken());
+        String token = jwtProvider.generateTokenWithUserName(refreshTokenRequest.getUsername());
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .refreshToken(refreshTokenRequest.getRefreshToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(refreshTokenRequest.getUsername())
+                .build();
     }
-
 }
