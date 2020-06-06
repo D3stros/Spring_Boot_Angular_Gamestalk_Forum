b6228577ee5cfe192014797f8f606746104d1a21
D3stros <patrick.fallmann@hotmail.com>
2020-06-06 14:49:21 +0200

    Changed pom.xml to be able to see exception messages in Postman

--- src/main/java/com/Gamestalk/springnggamestalk/model/User.java
+++ src/main/java/com/Gamestalk/springnggamestalk/model/User.java
@@ -3,13 +3,13 @@ package com.Gamestalk.springnggamestalk.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 
 import static javax.persistence.GenerationType.SEQUENCE;
