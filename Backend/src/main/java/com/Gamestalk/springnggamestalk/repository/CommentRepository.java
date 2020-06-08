bfdec045b32275f1e9f3b13939fcebcfd8823bc6
D3stros <patrick.fallmann@hotmail.com>
2020-06-08 23:45:30 +0200

    Added @repository annotation for repository classes

--- Backend/src/main/java/com/Gamestalk/springnggamestalk/repository/CommentRepository.java
+++ Backend/src/main/java/com/Gamestalk/springnggamestalk/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import com.Gamestalk.springnggamestalk.model.Comment;
 import com.Gamestalk.springnggamestalk.model.Post;
 import com.Gamestalk.springnggamestalk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
 
--- Backend/src/main/java/com/Gamestalk/springnggamestalk/repository/PostRepository.java
+++ Backend/src/main/java/com/Gamestalk/springnggamestalk/repository/PostRepository.java
@@ -4,9 +4,11 @@ import com.Gamestalk.springnggamestalk.model.Post;
 import com.Gamestalk.springnggamestalk.model.Topic;
 import com.Gamestalk.springnggamestalk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByTopic(Topic topic);
 
