0eb840bd1c84b7941266ae73fc93ee92f69b247b
D3stros <patrick.fallmann@hotmail.com>
2020-06-08 23:15:09 +0200

    Changed Post javax persistence type to identity

--- Backend/src/main/java/com/Gamestalk/springnggamestalk/model/Post.java
+++ Backend/src/main/java/com/Gamestalk/springnggamestalk/model/Post.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 import static javax.persistence.FetchType.LAZY;
-import static javax.persistence.GenerationType.SEQUENCE;
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
@@ -20,7 +20,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
 @NoArgsConstructor
 public class Post {
     @Id
-    @GeneratedValue(strategy = SEQUENCE)
+    @GeneratedValue(strategy = IDENTITY)
     private Long postId;
     @NotBlank(message = "Post Name cannot be empty or Null")
     private String postName;
@@ -29,7 +29,7 @@ public class Post {
     @Nullable
     @Lob
     private String description;
-    private Integer voteCount;
+    private Integer voteCount = 0;
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "userId", referencedColumnName = "userId")
     private User user;
